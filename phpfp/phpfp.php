40519854a6e3b90629da11ec9697f9fee3bb6eff
Dan McCarthy <dev@finii.com>
2013-04-02 01:14:46 -0400

    Fixing phpfp subfolder

---
+++ phpfp/phpfp.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="phpfp/fp.js"></script>
+<span style="display:none">Leave this field empty <input id="fp<?php echo uniqid();?>" type="text" name="formfield1234567894" value="" /></span>
+<input type="hidden" name="st" value="<?php echo time()+14921; ?>" /><br />
+<input id="fp<?php echo uniqid();?>" type="hidden" name="formfield1234567891" class="cffp_mm" value="" /><br />
+<input id="fp<?php echo uniqid();?>" type="hidden" name="formfield1234567892" class="cffp_kp" value="" /><br />
